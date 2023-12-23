1ab2fc4748cf52b1ca721b79b5671ad9a94fa235
Paul Huang <35037130+huanghuikang@users.noreply.github.com>
2023-12-23 11:40:42 +0800

    Delete SpringBootMatisPlus/SpringBootMatisPlus/src/main directory

--- SpringBootMatisPlus/SpringBootMatisPlus/src/main/java/cn/paul/plus/common/ResultGenerator.java
+++
@@ -1,9 +0,0 @@
-package cn.paul.plus.common;
-
-/**
- * @Author Paul Huang
- * @Purpose 目的
- * @Created 2023/12/19
- */
-public class ResultGenerator {
-}
--- SpringBootMatisPlus/SpringBootMatisPlus/src/main/java/cn/paul/plus/service/UserService.java
+++
@@ -1,6 +0,0 @@
-package cn.paul.plus.service;/**
- * @Author Paul Huang
- * @Purpose 目的
- * @Created 2023/12/18
- */public class UserService {
-}
